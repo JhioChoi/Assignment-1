f1ef72b1317b2fc2eda7caff1e8047bf677ee490
Jiho Choi <jchoi156@my.centennialcollege.ca>
2022-10-02 22:47:38 -0400

    initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,16 @@
+// IIFE -- Immediately Invoked Function Expression
+/*
+    File name : app.js
+    Name : Jiho Choi
+    Student Number : 301183378
+    Date : October 02, 2022
+*/ 
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+})();
